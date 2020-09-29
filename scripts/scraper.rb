8728284131bf7e4c0da8d1a228e7b2a92e650160
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-09-29 12:00:48 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,26 +1,15 @@
-before_action :fetch_current_category, only: [:show,:edit,:update] 
-before_action :fetch_categories, only: [:show,:edit,:update] 
-before_action :fetch_search_results, only: [:show,:edit,:update] #or whatever
-
-def show
-  rate
+users.each_with_index do |u, index|
+  # some code
+  if index == users.size - 1
+    # code for the last user
+  end
 end
 
-private
-
-def fetch_current_category
-  @category = Category.friendly.find(params[:id])
-end
-
-def fetch_categories
-  @categories = Category.all
-end
-
-def fetch_search_results
-  @search = category.products.approved.order(updated_at: :desc).ransack(params[:q])
-  @products = @search.result.page(params[:page]).per(50)
-end
-
-def largest_hash_key(hash)
-  hash.max_by{|k,v| v}
-end
+development:
+  adapter: mysql2
+  encoding: utf8
+  database: my_db_name
+  username: root
+  password: my_password
+  host: 127.0.0.1
+  port: 3306
