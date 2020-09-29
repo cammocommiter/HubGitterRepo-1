fa48cdca1cede2c6c1e42983823726af2ff5e8fd
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-09-29 12:00:50 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,15 +1,2 @@
-users.each_with_index do |u, index|
-  # some code
-  if index == users.size - 1
-    # code for the last user
-  end
-end
-
-development:
-  adapter: mysql2
-  encoding: utf8
-  database: my_db_name
-  username: root
-  password: my_password
-  host: 127.0.0.1
-  port: 3306
+attributes
+z.round(2)
