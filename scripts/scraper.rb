59e30c5f982c9f925c89da683dacd1da442d9c75
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-09-30 15:34:50 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,17 +1,2 @@
-f.input :color, :as => :select, :collection => Wine.colors.keys.to_a
-module GTranslate
-  class Translator
-    def perform( text ); translate( text ); end
-
-    private
-
-    def translate( text )
-      # do some private stuff here
-    end
-  end
-
-  def self.translate( text )
-    t = Translator.new
-    t.perform( text )
-  end
-end
+float
+!=
