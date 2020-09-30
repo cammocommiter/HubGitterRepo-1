4862e858ccb8574b35a57a23b20086e3d7ae659f
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-09-30 15:34:37 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,2 +1,8 @@
-json_string
-array = array.uniq
+uuid-ossp
+class Float
+  def self.times(&block)
+    self.to_i.times { |i| yield(i) }
+    remainder = self - self.to_i
+    yield(remainder) if remainder > 0.0
+  end
+end
