2b1135f381cab92c72b8431cdee0107f58202407
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-09-30 15:34:48 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,16 +1,17 @@
-remove_column :table_name, :column_name
+f.input :color, :as => :select, :collection => Wine.colors.keys.to_a
+module GTranslate
+  class Translator
+    def perform( text ); translate( text ); end
 
-namespace :thing do
-  desc "it does a thing"
-  task :work, [:option, :foo, :bar] do |task, args|
-    puts "work", args
-  end
-  
-  task :another, [:option, :foo, :bar] do |task, args|
-    puts "another #{args}"
-    Rake::Task["thing:work"].invoke(args[:option], args[:foo], args[:bar])
-    # or splat the args
-    # Rake::Task["thing:work"].invoke(*args)
+    private
+
+    def translate( text )
+      # do some private stuff here
+    end
   end
 
+  def self.translate( text )
+    t = Translator.new
+    t.perform( text )
+  end
 end
