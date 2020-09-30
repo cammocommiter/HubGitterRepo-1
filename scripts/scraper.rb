3735ecc3bfe323841ad3c23e3258f1ca24b9b116
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-09-30 15:34:39 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,8 +1,2 @@
-uuid-ossp
-class Float
-  def self.times(&block)
-    self.to_i.times { |i| yield(i) }
-    remainder = self - self.to_i
-    yield(remainder) if remainder > 0.0
-  end
-end
+/opt/local
+inspect
