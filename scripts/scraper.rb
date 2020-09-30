6c00e8b68207f63924c7df19f3d0475929882b3a
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-09-30 15:34:55 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,3 +1,23 @@
-Vagrantfile
-<%= javascript_include_tag "application" %>
-<%= yield :javascript_includes %>
+r or rb
+    Open file for reading.
+
+w or wb
+    Truncate to zero length or create file for writing.
+
+a or ab
+    Append; open or create file for writing at end-of-file.
+
+r+ or rb+ or r+b
+    Open file for update (reading and writing).
+
+w+ or wb+ or w+b
+    Truncate to zero length or create file for update.
+
+a+ or ab+ or a+b
+    Append; open or create file for update, writing at end-of-file.
+
+if ( (aa != nil && self.prop1 == aa.decrypt) ||
+   (bb != nil && self.prop2 == bb.decrypt) ) &&
+   (self.id.nil? || self.id != id)
+  return true
+end
