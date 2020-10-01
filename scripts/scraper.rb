a1ce4771b709c4a88b4ad3255401b6154ff351b1
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-01 21:43:50 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,3 +1,6 @@
 File.expand_path('../../Gemfile', __FILE__)
 
-if owner.is_a? thing //worked fine
+machine_bytes = ['foo'].pack('p').size
+machine_bits = machine_bytes * 8
+machine_max_signed = 2**(machine_bits-1) - 1
+machine_max_unsigned = 2**machine_bits - 1
