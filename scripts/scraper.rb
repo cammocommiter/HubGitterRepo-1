2bc803820710d6258a56a55b0105a1252148b43f
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-01 21:43:53 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,6 +1,2 @@
-File.expand_path('../../Gemfile', __FILE__)
-
-machine_bytes = ['foo'].pack('p').size
-machine_bits = machine_bytes * 8
-machine_max_signed = 2**(machine_bits-1) - 1
-machine_max_unsigned = 2**machine_bits - 1
+instance_variables
+validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
