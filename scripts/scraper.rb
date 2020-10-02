02a14c0ac48f832b17dbc104f84cddd12a890abc
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-02 13:59:29 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,17 +1,3 @@
-$ ruby system-stack-error.rb
-system-stack-error.rb:6:in `bar': stack level too deep (SystemStackError)
-        from system-stack-error.rb:2:in `foo'
-        from system-stack-error.rb:10:in `baz'
-        from system-stack-error.rb:6:in `bar'
-        from system-stack-error.rb:2:in `foo'
-        from system-stack-error.rb:10:in `baz'
-        from system-stack-error.rb:6:in `bar'
-        from system-stack-error.rb:2:in `foo'
-        from system-stack-error.rb:10:in `baz'
-         ... 10067 levels...
-        from system-stack-error.rb:10:in `baz'
-        from system-stack-error.rb:6:in `bar'
-        from system-stack-error.rb:2:in `foo'
-        from system-stack-error.rb:13:in `<main>'
+Hash[original_hash.to_a.sample(n)]
 
-skip_before_filter  :verify_authenticity_token
+devise
