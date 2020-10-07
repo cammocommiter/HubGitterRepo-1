3506a488ab7bd1cbb8db10e51f59c9a3e4efaf34
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-07 11:52:59 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,14 +1,9 @@
-namespace :db do
-  desc "Apply db tasks in custom databases, for example  rake db:alter[db:migrate,test-es] applies db:migrate on the database defined as test-es in databases.yml"
-  task :alter, [:task,:database] => [:environment] do |t, args|
-    require 'activerecord'
-    puts "Applying #{args.task} on #{args.database}"
-    ActiveRecord::Base.establish_connection(ActiveRecord::Base.configurations[args.database])
-    Rake::Task[args.task].invoke
-  end
-end
+all : usera userb userc....
 
-class Car < ActiveRecord::Base
-   belongs_to :vendor
-   belongs_to :company, :class_name => :Vendor,:foreign_key => "vendor_id"
-end
+usera:
+       imapsync usera
+userb:
+       imapsync userb
+....
+
+{}
