41fc04e2810657e27f7980bb97b6437562144dd7
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-07 11:53:06 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,5 +1,50 @@
-gem "therubyracer"
-gem "less-rails"
-gem "twitter-bootstrap-rails"
+require 'benchmark'
 
-Factory.after_ hooks
+class A
+  def test
+    10.times do
+      yield
+    end
+  end
+end
+
+class B
+  def test(&block)
+    10.times do
+      block.call
+    end
+  end
+end
+
+Benchmark.bm do |b|
+  b.report do
+    a = A.new
+    10000.times do
+      a.test{ 1 + 1 }
+    end
+  end
+
+  b.report do
+    a = B.new
+    10000.times do
+      a.test{ 1 + 1 }
+    end
+  end
+
+  b.report do
+    a = A.new
+    100000.times do
+      a.test{ 1 + 1 }
+    end
+  end
+
+  b.report do
+    a = B.new
+    100000.times do
+      a.test{ 1 + 1 }
+    end
+  end
+
+end
+
+return
