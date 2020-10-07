a9d6fb5ba931c477c01c93a111c1af19644dde13
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-07 12:30:27 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,5 +1,2 @@
-clients.select{|key, hash| hash["client_id"] == "2180" }
-#=> [["orange", {"client_id"=>"2180"}]]
-
->> File.join("path", "to", "join")
-=> "path/to/join"
+assert_raises
+File.expand_path('../../Gemfile', __FILE__)
