1f27d070699b181cdb6814ce5475f7edc1159c0d
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-09 10:33:08 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,6 +1,5 @@
-l
-# This gem will work with 1.8.6 or greater...
-spec.required_ruby_version = '>= 1.8.6'
+Rails.env == "production"
 
-# Only with ruby 2.0.x
-spec.required_ruby_version = '~> 2.0'
+Rails.env == "development"
+
+Rake::Task['your_task'].invoke
