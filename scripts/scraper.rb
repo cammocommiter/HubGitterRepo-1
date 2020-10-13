b04c725457a486e33589cd4753e668fd78c122c9
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-13 14:00:45 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,2 +1,9 @@
-true
-parameters
+FOO = 18
+private_constant :FOO
+
+string lastItem = list[list.Count - 1];
+foreach (string item in list) {
+    if (item != lastItem)
+        Console.WriteLine("Looping: " + item);
+    else    Console.Writeline("Lastone: " + item);
+}
