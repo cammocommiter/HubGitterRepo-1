8de19ec9df6f802b18f629631ac801fb8f04b97d
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-15 11:20:08 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,3 +1,16 @@
-brew install imagemagick --disable-openmp
+begin
+  this_will_fail!
+rescue Failure => error
+  log.error error.message
+  raise
+end
 
-puts " Total Revenue of East Cost: #{total_revenue_of_east_cost}"
+class Booking < ActiveRecord::Base
+  has_many   :booking_payments
+  ....
+  def destroy
+    raise "Cannot delete booking with payments" unless booking_payments.count == 0
+    # ... ok, go ahead and destroy
+    super
+  end
+end
