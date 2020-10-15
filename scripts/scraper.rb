6034a01afb09f377df5c081f7c909b407a5fa936
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-15 11:20:09 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,16 +1,2 @@
-begin
-  this_will_fail!
-rescue Failure => error
-  log.error error.message
-  raise
-end
-
-class Booking < ActiveRecord::Base
-  has_many   :booking_payments
-  ....
-  def destroy
-    raise "Cannot delete booking with payments" unless booking_payments.count == 0
-    # ... ok, go ahead and destroy
-    super
-  end
-end
+downcase
+StandardError
