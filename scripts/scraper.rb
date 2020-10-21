29e698c5f9e8c96db0d73cbd444b91fafeeb2ae1
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-21 08:28:11 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,6 +1,2 @@
-!
-ages = { 'Bruce' => 32, 'Clark' => 28 }
-mappings = { 'Bruce' => 'Bruce Wayne', 'Clark' => 'Clark Kent' }
-
-ages.transform_keys(&mappings.method(:[]))
-#=> { 'Bruce Wayne' => 32, 'Clark Kent' => 28 }
+from
+Enumerable#each_slice
