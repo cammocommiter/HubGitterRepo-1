ceb7b3821ecd82a785d38d5fd36a39f32e0c3aa9
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-11-03 19:18:54 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,3 +1,17 @@
-each_slice
-  # Custom directories with classes and modules you want to be autoloadable.
-  # config.autoload_paths += %W(#{config.root}/extras)
+class Hello
+  def create_some_state
+    @hello = "hello"
+  end
+end
+
+h = Hello.new
+p h.instance_variables 
+
+h.create_some_state
+p h.instance_variables
+
+# Output
+[]
+["@hello"]
+
+<%= render :partial => 'form', :locals => { :item => @item } %>
