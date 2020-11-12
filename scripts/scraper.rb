54079f2d89f14679e4b0131bde1017fe1c8b81da
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-11-13 07:55:05 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,13 +1,8 @@
-require 'digest/sha1'
-Digest::SHA1.hexdigest 'foo'
+module GemName
 
-def multiple_of(factor)
-  Proc.new{|product| product.modulo(factor).zero?}
+  class AuthenticationError < StandardError; end
+  class InvalidUsername < AuthenticationError; end
+
 end
 
-case number
-  when multiple_of(3)
-    puts "Multiple of 3"
-  when multiple_of(7)
-    puts "Multiple of 7"
-end
+sudo
