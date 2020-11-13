89e518505a4f0324e4a85c7154211f95a1552871
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-11-14 07:29:30 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,8 +1,6 @@
-module GemName
+["10", "20", "50","99"].map(&:inspect).join(', ') # => '"10", "20", "50", "99"'
 
-  class AuthenticationError < StandardError; end
-  class InvalidUsername < AuthenticationError; end
-
+CSV.foreach('my_file.csv', :headers => true) do |row|
+  puts row['foo'] # prints 1 the 1st time, "blah" 2nd time, etc
+  puts row['bar'] # prints 2 the first time, 7 the 2nd time, etc
 end
-
-sudo
