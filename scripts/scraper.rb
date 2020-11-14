92d8883c6275408882463efa065dc01105fd1520
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-11-15 07:30:25 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,6 +1,4 @@
-["10", "20", "50","99"].map(&:inspect).join(', ') # => '"10", "20", "50", "99"'
+h.sort_by {|k,v| v}.reverse
 
-CSV.foreach('my_file.csv', :headers => true) do |row|
-  puts row['foo'] # prints 1 the 1st time, "blah" 2nd time, etc
-  puts row['bar'] # prints 2 the first time, 7 the 2nd time, etc
-end
+>> "àáâãäå".mb_chars.normalize(:kd).gsub(/[^\x00-\x7F]/n,'').downcase.to_s
+=> "aaaaaa"
