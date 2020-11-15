f352870b09e5a151784a115d7902fcbbbee0686a
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-11-16 07:14:55 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,4 +1,20 @@
-h.sort_by {|k,v| v}.reverse
+instance_variable_get
+module MyModule
+  # self here is MyModule
+  @species = "frog"
+  @color = "red polka-dotted"
+  @log = []
 
->> "àáâãäå".mb_chars.normalize(:kd).gsub(/[^\x00-\x7F]/n,'').downcase.to_s
-=> "aaaaaa"
+  def self.log(msg)
+    # self here is still MyModule, so the instance variables are still available
+    @log << msg
+  end
+  def self.show_log
+    puts @log.map { |m| "A #@color #@species says #{m.inspect}" }
+  end
+end
+
+MyModule.log "I like cheese."
+MyModule.log "There's no mop!"
+MyModule.show_log #=> A red polka-dotted frog says "I like cheese."
+                  #   A red polka-dotted frog says "There's no mop!"
