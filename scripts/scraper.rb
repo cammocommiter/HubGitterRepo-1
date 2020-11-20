943c234e77fb54dfe17ce49fa87a095894c8d3a6
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-11-21 07:40:01 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,20 +1,3 @@
-instance_variable_get
-module MyModule
-  # self here is MyModule
-  @species = "frog"
-  @color = "red polka-dotted"
-  @log = []
+raise new.TriangleError
 
-  def self.log(msg)
-    # self here is still MyModule, so the instance variables are still available
-    @log << msg
-  end
-  def self.show_log
-    puts @log.map { |m| "A #@color #@species says #{m.inspect}" }
-  end
-end
-
-MyModule.log "I like cheese."
-MyModule.log "There's no mop!"
-MyModule.show_log #=> A red polka-dotted frog says "I like cheese."
-                  #   A red polka-dotted frog says "There's no mop!"
+ENV['COLUMNS']
