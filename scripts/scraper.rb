c3a54f3f775f8ebe3736220176de550be182ee42
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-12-22 07:48:24 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,9 +1,2 @@
-rbenv
-admins = User.where(:kind => :admin)
-authors = User.where(:kind => :author)
-
-admins = admins.where_values.reduce(:and)
-authors = authors.where_values.reduce(:and)
-
-User.where(admins.or(authors)).to_sql
-# => "SELECT \"users\".* FROM \"users\"  WHERE ((\"users\".\"kind\" = 'admin' OR \"users\".\"kind\" = 'author'))"
+rails generate rspec:install
+File.delete(path_to_file) if File.exist?(path_to_file)
