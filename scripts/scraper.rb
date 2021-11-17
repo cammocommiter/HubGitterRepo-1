5d13de65ebe58af8b740d0a0baa33ec432808bbc
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2021-11-17 17:31:24 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,3 +1,7 @@
-ruby -e 'puts $:'
-
-~/.rvm/gemsets/global.gems
+%q
+irb(main):007:0> :test.object_id
+=> 83618
+irb(main):008:0> :test.object_id
+=> 83618
+irb(main):009:0> :test.object_id
+=> 83618
