2332ec7a94b6003e5e5367d3ba03ce00c2caaf2f
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2022-01-22 03:02:10 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,2 +1,7 @@
-/bin/bash --login; source ~/.rvm/scripts/rvm; rvm use 2.4.0
-% rake test TEST=test/test_foobar.rb TESTOPTS="--name=test_foobar1 -v"
+str = "\x12\x34\x56\x78\x9a\xbc\xde\xf1\x23\x45\x67\x89\xab\xcd\xef\x12\x34\x56\x78\x9a".force_encoding('ASCII-8BIT')
+puts CGI.escape str
+
+
+=> "%124Vx%9A%BC%DE%F1%23Eg%89%AB%CD%EF%124Vx%9A"
+
+Rails3
